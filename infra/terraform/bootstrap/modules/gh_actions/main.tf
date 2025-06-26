9f9b7e390ed0e0d75438787ae66c2997b79406b2
mateus barbeiro <mateusgarcia2001@gmail.com>
2025-06-25 22:56:09 -0300

    [BARUAM-24] Configurar CI/CD git hub actions - ajustes

--- infra/terraform/bootstrap/modules/gh_actions/main.tf
+++ infra/terraform/bootstrap/modules/gh_actions/main.tf
@@ -23,6 +23,13 @@ resource "google_project_iam_member" "github_sa_project_viewer" {
 # IAM para o GitHub Actions permissão de leitura no bucket para o Terraform State
 resource "google_storage_bucket_iam_member" "terraform_backend_writer_reader" {
   bucket = var.terraform_bootstrap_state_bucket_name
+  role   = "roles/storage.admin"
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+# IAM para o GitHub Actions permissão de admin no bucket para o Terraform State
+resource "google_storage_bucket_iam_member" "terraform_backend_object_admin" {
+  bucket = var.terraform_bootstrap_state_bucket_name
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.github_actions.email}"
 }
